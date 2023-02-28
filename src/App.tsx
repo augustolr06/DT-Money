44f1351093d287bac1c267d0aca781584a6dc398
Augusto Lázaro <augusto.lr06@gmail.com>
2023-02-28 11:50:08 -0300

    feat: create transactions table

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
-import { Transations } from './pages/Transations/Transations'
+import { Transations } from './pages/Transactions/Transactions'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
---
+++ src/pages/Transactions/Transactions.tsx
@@ -0,0 +1,39 @@
+import { Header } from '../../components/Header/Header'
+import { Summary } from '../../components/Summary/Summary'
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './Transactions.styles'
+
+export function Transations() {
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hambugueria</td>
+              <td>
+                <PriceHighlight variant="outcome"> - R$ 59,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>13/04/2022</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  )
+}
